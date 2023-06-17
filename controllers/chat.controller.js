6e9d59ed8632f523c143c856443564fffeb50c59
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-06-18 01:43:26 +0700

    done

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,23 +1,24 @@
-import { Configuration, OpenAIApi } from "openai";
-
-const openAIConfig = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(openAIConfig);
+import axios from "axios";
 
 export const chatCompletion = async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const answer = await openai.createCompletion({
-      model: "gpt-3.5-turbo-16k-0613",
-      prompt,
-      temperature: 0,
-      maxTokens: 3000,
-    });
+    const answer = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo-16k-0613",
+        messages: [{ role: "user", content: prompt }],
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+      }
+    );
 
-    const text = answer.data.choices[0].text;
+    const text = answer.data.choices[0].message.content;
 
     res.status(200).json({ text });
   } catch (error) {
